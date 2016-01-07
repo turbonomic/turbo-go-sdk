446c6a9f02b4d0b5ee63f705c79198bbb70e101a
Pamela Sanchez <pamela.aimee.sanchez@gmail.com>
2016-01-07 13:48:55 -0800

    completed tests in server_entity_prob_def_constant_test.go

--- sdk/server_entity_prob_def_constant_test.go
+++ sdk/server_entity_prob_def_constant_test.go
@@ -15,3 +15,14 @@ func TestgetIpHandler(t *testing.T) {
 	assert.Equal(false, *propertyHandler.DirectlyApply)
 	assert.Equal("getAddress", *propertyHandler.MethodName)
 }
+
+// Test that the Entity and PropertyHandler member variables of
+// the struct returned bu getVirtualMachineIpProperty are not nil
+// tests that the attribute string type member variable is "UsesEndPoints"
+func TestgetVirtualMachineIpProperty(t *testing.T) {
+	assert := assert.New(t)
+	serverEntityProp := getVirtualMachineIpProperty()
+	assert.Equal("UsesEndPoints", *serverEntityProp.Attribute)
+	assert.NotNil(serverEntityProp.Entity)
+	assert.NotNil(serverEntityProp.PropertyHandler)
+}
