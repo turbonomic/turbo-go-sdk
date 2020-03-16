bc43f3c3c573aa0b022d7637a980831638a49fe0
enlin_xu <enlin.xu@turbonomic.com>
2020-03-15 23:22:45 -0400

    Fixed fmt for two irrelevant files in protobuf

--- pkg/proto/CommonDTO.pb.go
+++ pkg/proto/CommonDTO.pb.go
@@ -4781,7 +4781,8 @@ type EntityDTO_StorageControllerData_IopsComputeData struct {
 
 func (*EntityDTO_StorageControllerData_IopsCapacity) isEntityDTO_StorageControllerData_IopsData() {}
 
-func (*EntityDTO_StorageControllerData_IopsComputeData) isEntityDTO_StorageControllerData_IopsData() {}
+func (*EntityDTO_StorageControllerData_IopsComputeData) isEntityDTO_StorageControllerData_IopsData() {
+}
 
 func (m *EntityDTO_StorageControllerData) GetIopsData() isEntityDTO_StorageControllerData_IopsData {
 	if m != nil {
@@ -8307,8 +8308,10 @@ type GroupDTO_SelectionSpec_PropertyStringList struct {
 func (m *GroupDTO_SelectionSpec_PropertyStringList) Reset() {
 	*m = GroupDTO_SelectionSpec_PropertyStringList{}
 }
-func (m *GroupDTO_SelectionSpec_PropertyStringList) String() string { return proto.CompactTextString(m) }
-func (*GroupDTO_SelectionSpec_PropertyStringList) ProtoMessage()    {}
+func (m *GroupDTO_SelectionSpec_PropertyStringList) String() string {
+	return proto.CompactTextString(m)
+}
+func (*GroupDTO_SelectionSpec_PropertyStringList) ProtoMessage() {}
 func (*GroupDTO_SelectionSpec_PropertyStringList) Descriptor() ([]byte, []int) {
 	return fileDescriptor_2233245ab49fede8, []int{4, 7, 0}
 }
@@ -8349,8 +8352,10 @@ type GroupDTO_SelectionSpec_PropertyDoubleList struct {
 func (m *GroupDTO_SelectionSpec_PropertyDoubleList) Reset() {
 	*m = GroupDTO_SelectionSpec_PropertyDoubleList{}
 }
-func (m *GroupDTO_SelectionSpec_PropertyDoubleList) String() string { return proto.CompactTextString(m) }
-func (*GroupDTO_SelectionSpec_PropertyDoubleList) ProtoMessage()    {}
+func (m *GroupDTO_SelectionSpec_PropertyDoubleList) String() string {
+	return proto.CompactTextString(m)
+}
+func (*GroupDTO_SelectionSpec_PropertyDoubleList) ProtoMessage() {}
 func (*GroupDTO_SelectionSpec_PropertyDoubleList) Descriptor() ([]byte, []int) {
 	return fileDescriptor_2233245ab49fede8, []int{4, 7, 1}
 }
