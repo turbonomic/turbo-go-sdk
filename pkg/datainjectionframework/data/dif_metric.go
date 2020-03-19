57a39e373094523c3c6ed04d68929faeef65ead0
pallavi_debnath <pallavi.debnath@turbonomic.com>
2020-03-19 09:15:34 -0400

    Data Injection framework topology schema and data structures

---
+++ pkg/datainjectionframework/data/dif_metric.go
@@ -0,0 +1,46 @@
+package data
+
+type CDPMetric struct {
+	MetricMap map[string]*CDPMetricVal
+}
+
+
+type CDPMetricVal struct {
+	Average     float64       `json:"average"`
+	Min         float64       `json:"min"`
+	Max         float64       `json:"min"`
+	Capacity    float64       `json:"min"`
+	Unit        CDPMetricUnit `json:"unit"`
+	Key         string        `json:"key"`
+	Description string        `json:"description"`
+	RawMetrics  interface{}   `json:"rawData"`
+}
+
+type CDPMetricUnit string
+
+const (
+	COUNT CDPMetricUnit = "count"
+	TPS   CDPMetricUnit = "tps"
+	MS    CDPMetricUnit = "ms"
+	MB    CDPMetricUnit = "mb"
+	MHZ   CDPMetricUnit = "mhz"
+	PCT   CDPMetricUnit = "pct"
+)
+
+type CDPMetricValKey string
+
+const (
+	KEY         CDPMetricValKey = "key"
+	DESCRIPTION CDPMetricValKey = "description"
+	RAWDATA     CDPMetricValKey = "rawData"
+	AVERAGE     CDPMetricValKey = "average"
+	MAX         CDPMetricValKey = "max"
+	MIN         CDPMetricValKey = "min"
+	CAPACITY    CDPMetricValKey = "capacity"
+	UNIT        CDPMetricValKey = "unit"
+)
+
+const (
+	UNSET_FLOAT  = -100.0
+	UNSET_STRING = ""
+)
