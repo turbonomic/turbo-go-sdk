6878b3bf419b7ef1efbfff57ccb2640c0867243e
meng_ding <meng.ding@turbonomic.com>
2020-04-16 18:50:50 -0400

    Update builder function

--- pkg/dataingestionframework/data/dif_types.go
+++ pkg/dataingestionframework/data/dif_types.go
@@ -31,6 +31,7 @@ type DIFHostedOn struct {
 type DIFPartOf struct {
 	ParentEntity string `json:"entity"`
 	UniqueId     string `json:"uniqueId"`
+	Label        string `json:"label,omitempty"`
 }
 
 func NewDIFEntity(uid, eType string) *DIFEntity {
@@ -49,12 +50,12 @@ func (e *DIFEntity) WithName(name string) *DIFEntity {
 	return e
 }
 
-func (e *DIFEntity) PartOfEntity(entity, id string) *DIFEntity {
+func (e *DIFEntity) PartOfEntity(entity, id, label string) *DIFEntity {
 	if e.partOfSet.Contains(id) {
 		return e
 	}
 	e.partOfSet.Add(id)
-	e.PartOf = append(e.PartOf, &DIFPartOf{entity, id})
+	e.PartOf = append(e.PartOf, &DIFPartOf{entity, id, label})
 	return e
 }
 
@@ -105,7 +106,6 @@ func (e *DIFEntity) Matching(id string) *DIFEntity {
 }
 
 func (e *DIFEntity) AddMetric(metricType string, key DIFMetricValKey, value float64) {
-	var meList []*DIFMetricVal
 	meList, found := e.Metrics[metricType]
 	if !found {
 		meList = append(meList, &DIFMetricVal{})
@@ -119,6 +119,10 @@ func (e *DIFEntity) AddMetric(metricType string, key DIFMetricValKey, value floa
 	} else if key == CAPACITY {
 		meList[0].Capacity = &value
 	}
+}
+
+func (e *DIFEntity) AddMetrics(metricType string, metricVals []*DIFMetricVal) {
+	e.Metrics[metricType] = append(e.Metrics[metricType], metricVals...)
 }
 
 func (e *DIFEntity) String() string {
@@ -136,6 +140,11 @@ func (e *DIFEntity) String() string {
 		s += fmt.Sprintf(" HostedOn")
 		s += fmt.Sprintf("[%s:%s]",
 			e.HostedOn.HostUuid, e.HostedOn.IPAddress)
+	}
+	for metricName, metricList := range e.Metrics {
+		for _, metric := range metricList {
+			s += fmt.Sprintf(" Metric %s:[%v]", metricName, metric)
+		}
 	}
 	return s
 }
